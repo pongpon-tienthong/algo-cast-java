aa7c67f2bfec738d6f747b5e19bfa6973c591848
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-12-18 16:13:10 -0600

    Chunk Done

--- src/chunk/Chunk.java
+++ src/chunk/Chunk.java
@@ -1,7 +1,7 @@
 package chunk;
 
 // --- Directions
-// Given an array and chunk size, divide the array into many subarrays
+// Given an array and chunk size, divide the array Integero many subarrays
 // where each subarray is of length size
 // --- Examples
 // chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
@@ -10,6 +10,31 @@ package chunk;
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-public class Chunk {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Chunk {
+    public ArrayList<ArrayList<Integer>> chunk(Integer[] arr, Integer size) {
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+
+        for (Integer ele: arr) {
+
+            if (res.isEmpty()) {
+                res.add(new ArrayList<>(Arrays.asList(ele)));
+                continue;
+            }
+
+            ArrayList<Integer> last = res.get(res.size() - 1);
+
+            if(last.size() == size){
+                res.add(new ArrayList<>(Arrays.asList(ele)));
+            } else {
+                last.add(ele);
+            }
+        }
+        
+        return res;
+    }
 }
--- src/chunk/ChunkTest.java
+++ src/chunk/ChunkTest.java
@@ -1,7 +1,73 @@
 package chunk;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
 
 public class ChunkTest {
 
+    private Chunk chunk;
+
+    @BeforeEach
+    void setUp() {
+        chunk = new Chunk();
+    }
+
+    @Test
+    public void test_chunk_divides_an_array_of_10_elements_with_chunk_size_2() {
+        Integer[] arr = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Integer size = 2;
+
+        ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>((Arrays.asList(1, 2))));
+        expected.add(new ArrayList<>((Arrays.asList(3, 4))));
+        expected.add(new ArrayList<>((Arrays.asList(5, 6))));
+        expected.add(new ArrayList<>((Arrays.asList(7, 8))));
+        expected.add(new ArrayList<>((Arrays.asList(9, 10))));
+
+        assertIterableEquals(expected, chunk.chunk(arr, size));
+    }
+
+    @Test
+    public void test_chunk_divides_an_array_of_3_elements_with_chunk_size_1() {
+        Integer[] arr = new Integer[]{1, 2, 3};
+        Integer size = 1;
+
+        ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>((Arrays.asList(1))));
+        expected.add(new ArrayList<>((Arrays.asList(2))));
+        expected.add(new ArrayList<>((Arrays.asList(3))));
+
+        assertIterableEquals(expected, chunk.chunk(arr, size));
+    }
+
+    @Test
+    public void test_chunk_divides_an_array_of_5_elements_with_chunk_size_3() {
+        Integer[] arr = new Integer[]{1, 2, 3, 4, 5};
+        Integer size = 3;
+
+        ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>((Arrays.asList(1, 2, 3))));
+        expected.add(new ArrayList<>((Arrays.asList(4, 5))));
+
+        assertIterableEquals(expected, chunk.chunk(arr, size));
+    }
+
+    @Test
+    public void test_chunk_divides_an_array_of_13_elements_with_chunk_size_5() {
+        Integer[] arr = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        Integer size = 5;
+
+        ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>((Arrays.asList(1, 2, 3, 4, 5))));
+        expected.add(new ArrayList<>((Arrays.asList(6, 7, 8, 9, 10))));
+        expected.add(new ArrayList<>((Arrays.asList(11, 12, 13))));
+
+        assertIterableEquals(expected, chunk.chunk(arr, size));
+    }
 }
